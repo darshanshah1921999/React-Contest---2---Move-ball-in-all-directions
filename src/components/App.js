fd566f6b3c3099b35752d0d8a09c02623def3822
darshanshah1921999 <54939839+darshanshah1921999@users.noreply.github.com>
2020-10-25 19:28:17 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import React, { Component, useState } from "react";
-import "../styles/App.css";
+import React, { Component, useEffect, useState } from "react";
+import "./styles.css";
 
 const App = () => {
   const [renderBall, setRenderBall] = useState(false);
@@ -9,7 +9,10 @@ const App = () => {
     left: "0px",
     top: "0px"
   });
-  const updatePosition = () => {
+  const updatePosition = (x, y) => {
+    console.log(x);
+    setX(x);
+    setY(y);
     const updatedPosition = {
       left: x + "px",
       top: y + "px"
@@ -17,36 +20,53 @@ const App = () => {
     // console.log(updatedPosition);
     setBallPosition(updatedPosition);
   };
+
+  const clickHandler = (event) => {
+    if (event.keyCode === 39) {
+      updatePosition(x + 5, y);
+    } else if (event.keyCode === 40) {
+      updatePosition(x, y + 5);
+    } else if (event.keyCode === 38) {
+      updatePosition(x, y - 5);
+    } else if (event.keyCode === 37) {
+      updatePosition(x - 5, y);
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("keydown", clickHandler);
+
+    return () => {
+      document.removeEventListener("keydown", clickHandler);
+    };
+  }, []);
+
+  React.useEffect(() => {
+    document.addEventListener("keydown", clickHandler);
+    return () => {
+      document.removeEventListener("keydown", clickHandler);
+    };
+  }, [x]);
+
+  React.useEffect(() => {
+    document.addEventListener("keydown", clickHandler);
+    return () => {
+      document.removeEventListener("keydown", clickHandler);
+    };
+  }, [y]);
   const buttonClickHandler = () => {
     setRenderBall(true);
-    document.addEventListener("keydown", (event) => {
-      if (event.keyCode === 39) {
-        const copy = x + 5;
-        setX(copy);
-        updatePosition();
-      } else if (event.keyCode === 40) {
-        setY(y - 5);
-        updatePosition();
-      } else if (event.keyCode === 38) {
-        setY(y + 5);
-        updatePosition();
-      } else if (event.keyCode === 37) {
-        setX(x - 5);
-        updatePosition();
-      }
-    });
   };
   const reset = () => {
-    setX(0);
-    setY(0);
-    updatePosition();
+    updatePosition(0, 0);
   };
   const renderChoice = () => {
     if (renderBall) {
       return <div className="ball" style={ballPosition}></div>;
     } else
       return (
-        <button onClick={() => buttonClickHandler()}>Click For One Ball</button>
+        <button className="start" onClick={() => buttonClickHandler()}>
+          Click For One Ball
+        </button>
       );
   };
 
